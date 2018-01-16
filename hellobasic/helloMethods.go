74b4bef9796ec5b4c48eb64219fa21b6a49ad2b2
SebLKMa <sebmalikkeung@gmail.com>
2018-01-16 21:38:48 +0800

    lowercase type name denotes unexported type

--- hellobasic/helloMethods.go
+++ hellobasic/helloMethods.go
@@ -4,29 +4,30 @@ import (
 	"fmt"
 )
 
-type User struct {
+// lowercase type name denotes unexported type
+type user struct {
 	name  string
 	email string
 }
 
 // A method wuth a value receiver (u User)
-func (u User) notify() {
+func (u user) notify() {
 	fmt.Printf("Sending notification to %s<%s>\n", u.name, u.email)
 }
 
 // A method wuth a pointer receiver (u *User)
-func (u *User) changeEmail(newEmail string) {
+func (u *user) changeEmail(newEmail string) {
 	u.email = newEmail
 }
 
 func main() {
 	// Values of type User can be used to call methods
 	// declared with a value receiver
-	mona := User{"Mona", "mona@somewhere.com"}
+	mona := user{"Mona", "mona@somewhere.com"}
 	mona.notify()
 
 	// lisa is declared and initialized as a Pointer to an address of type User
-	lisa := &User{"Lisa", "lisa@nowhere.com"}
+	lisa := &user{"Lisa", "lisa@nowhere.com"}
 	lisa.notify()
 
 	// Values of type User can be used to call methods declared with
